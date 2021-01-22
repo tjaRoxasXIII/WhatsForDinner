3e06e0941d77026ae03118c3737195545d66a488
Taylor <tjaroguetech@gmail.com>
2021-01-22 13:54:35 -0500

    Testing implementation of ActionMailer on creation of a new user

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -18,13 +18,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      payload = {user_id: @user.id, email: @user.email}
-      token = JWT.encode(payload, ENV['SECRET_KEY'])
-      render json: {user: @user, token: token}, status: :created
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+    # respond_to do |format|
+      if @user.save
+        UserMailer.with(user: @user).welcome_email.deliver_now
+        payload = {user_id: @user.id, email: @user.email}
+        token = JWT.encode(payload, ENV['SECRET_KEY'])
+        render json: {user: @user, token: token}, status: :created
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
   end
 
   # PATCH/PUT /users/1
@@ -49,6 +51,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -13,7 +13,8 @@
 ActiveRecord::Schema.define(version: 2021_01_09_185540) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
