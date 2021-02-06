c783c691ecf5f60584f4b51214fbbb08b29ada46
Taylor <tjaroguetech@gmail.com>
2021-02-06 13:52:38 -0500

    Removing eslintcache from repo and adding to my gitignore

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
 export default function Home() {
+
+    const signOut = (e) => {
+        localStorage.removeItem("token")
+    }
+
     return (
         <div className="App">
-            <h1 className="header">Logged in!</h1>
+            <h2 className="header">What's for dinner?
+                <button className="Options" onClick={signOut}>Sign Out</button>
+            </h2>
+
         </div>
     )
 } 
