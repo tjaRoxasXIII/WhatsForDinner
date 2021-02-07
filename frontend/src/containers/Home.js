1e24c11d1b64103b34d057c17337aee383c48da3
Taylor <tjaroguetech@gmail.com>
2021-02-07 14:04:43 -0500

    Made a minor UI change and began adding in the fetch request to grab a user's recipes on sign-in

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,10 +1,17 @@
-import React from 'react'
-// import { Redirect } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 export default function Home() {
-    const yourName = useSelector(state => state.auth.name) 
+    const yourName = useSelector(state => state.auth.name)
+    const yourID = useSelector(state => state.auth.userId)
+    const [yourRecipes, setYourRecipes] = useState({recipes: []})
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/users/${yourID}/recipes`)
+            .then(resp => resp.json())
+            .then(data => setYourRecipes(data.data))
+    }, [])
 
     const signOut = (e) => {
         localStorage.removeItem("token")
